ed62125ec61a12c6694c16d3fbb260e4801979a8
hz081 <hantze.sudarma@gmail.com>
2021-02-04 09:32:21 +0700

    add: parse args from param function

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-main = () => {
+const { exit } = require('process');
 
+const parseArgument = (args) => {
+    let result = args.slice(2);
+    if (result.length < 1) throw Error();
+    return result[0];
+}
+
+main = () => {
+    let args, data;
+    try {
+        args = parseArgument(process.argv);
+    } catch (e) {
+        console.log('cannot parse the arguments', e);
+        exit(1);
+    }
 }
 
 main();
