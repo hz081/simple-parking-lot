cf9c34cd08263c958f04f62e73ea1f2c7526c9fa
hz081 <hantze.sudarma@gmail.com>
2021-02-04 09:22:22 +0700

    add: read file function from argument parameter

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ const parseArgument = (args) => {
 }
 
 main = () => {
-    let args;
+    let args, data, lines;
 
     try {
         args = parseArgument(process.argv);
     } catch (e) {
         console.log('cannot parse the arguments', e);
+        exit(1);
+    }
+
+    try {
+        data = fs.readFileSync(args, 'utf8');
+        lines = data.split('\n');
+    } catch(e) {
+        console.log('error while reading the data', e);
         exit(1);
     }
 }
