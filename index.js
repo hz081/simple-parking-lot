bb2f537241b1ad5ab862332e8160d6c41fa16507
hz081 <hantze.sudarma@gmail.com>
2021-02-04 09:33:23 +0700

    add: read data from args file param function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { exit } = require('process');
+const fs = require('fs');
 
 const parseArgument = (args) => {
     let result = args.slice(2);
@@ -12,6 +13,13 @@ main = () => {
         args = parseArgument(process.argv);
     } catch (e) {
         console.log('cannot parse the arguments', e);
+        exit(1);
+    }
+    try {
+        data = fs.readFileSync(args, 'utf8');
+        lines = data.split('\n');
+    } catch(e) {
+        console.log('error while reading the data', e);
         exit(1);
     }
 }
